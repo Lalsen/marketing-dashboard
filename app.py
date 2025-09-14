31552e407270e54979da2e4432c466e4d4bd2bc7
Lalsen <118272686+Lalsen@users.noreply.github.com>
2025-09-15 02:34:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 @st.cache_data
 def load_data():
-    fb = pd.read_csv("data/Facebook.csv")
+    fb = pd.read_csv("data/facebook.csv")
     google = pd.read_csv("data/Google.csv")
     tiktok = pd.read_csv("data/TikTok.csv")
     business = pd.read_csv("data/Business.csv")
